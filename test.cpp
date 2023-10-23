0834a486f1fec77f5d0e6f5af92d29f3dbb38b23
huy nguyen <nguyenquochuy3102@gmail.com>
2023-10-23 15:07:57 +0700

    stt 28 sua file test.cpp (phan conflict)

--- test.cpp
+++ test.cpp
@@ -1,24 +1,25 @@
 ////2051052058_NguyenQuocHuy_BT03
-//#include <iostream>
-//#include <string>
-//#include <iomanip>
-//using namespace std;
-//int main()
-//{ 
-// string tens;
-// int soNgay;
-// double tienCong;
-// double tienLuong;
-// cout <<"nhap tens: ";
-// getline ( cin, ten );
-// cout << "nhap so ngay slam viec: ";
-// cin >> soNgay;
-// cout <<" nhap tien cong 1 ngay: ";
-// cin >>tienCong;
-// tienLuong = soNgay * tienCong;
-// cout <<"Ten:" << ten << endsl;
-// cout <<" tien luong huy :" << fixed << setprecision (0) 
-//	 << tienLuong
-// system ("pause");
-// return 0;
-//}
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main()
+{ 
+ string tens;
+ int soNgay;
+ double tienCong;
+ double tienLuong;
+ cout <<"nhap ten: ";
+ getline ( cin, ten );
+ cout << "nhap so ngay lam viec: ";
+ cin >> soBuoi;
+
+ cout <<" nhap tien cong 1 ngay: ";
+ cin >>tienCong;
+ tienLuong = soBuoi * tienCong;
+ cout <<"Ten:" << ten << endsl;
+ cout <<" tien luong huy :" << fixed << setprecision (0) 
+	 << tienLuong
+ system ("pause");
+ return 0;
+}
